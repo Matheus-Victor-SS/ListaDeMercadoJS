14e7c49231469c372547f9eedf0bf5f482194b78
Matheus <matheus.victor.ss48@gmail.com>
2026-03-02 09:31:20 -0300

    após 15 produtos sera mudado para a proxima pagina, que pode ser mudada quando quiser, além de retirar a edição por alerta

--- script.js
+++ script.js
@@ -1,72 +1,104 @@
- var Lista = [];
+let lista = [];
+let paginaAtual = 1;
+const itensPorPagina = 15;
 
-function PegarValor(){
+function adicionarItem(){
 
-    var nomeInput = document.getElementById("nome");
-    var qtdInput = document.getElementById("quantidade");
+    let nomeInput = document.getElementById("nome");
+    let qtdInput = document.getElementById("quantidade");
 
-    var nome = nomeInput.value.trim();
-    var qtd = qtdInput.value.trim();
+    let nome = nomeInput.value.trim();
+    let qtd = qtdInput.value.trim();
 
     if(nome !== "" && qtd !== ""){
 
-        var itemCompleto = nome + " - " + qtd + "x";
+        lista.push({nome: nome, qtd: qtd});
+        nomeInput.value = "";
+        qtdInput.value = "";
 
-        if(!Lista.includes(itemCompleto)){
-            Lista.push(itemCompleto);
-
-            const lista = document.getElementById("ListaNomes");
-            const item = document.createElement("li");
-            item.textContent = itemCompleto;
-
-            // BOTÃO EDITAR
-            const botaoEditar = document.createElement("button");
-            botaoEditar.innerHTML = '<i class="fa-solid fa-pen"></i>';
-
-            botaoEditar.onclick = function(){
-
-                item.classList.add("editando");
-
-                var novoNome = prompt("Editar produto:", nome);
-                var novaQtd = prompt("Editar quantidade:", qtd);
-
-                if(novoNome && novaQtd){
-
-                    var novoItem = novoNome.trim() + " - " + novaQtd.trim() + "x";
-                    item.firstChild.textContent = novoItem;
-
-                    Lista = Lista.map(valor =>
-                        valor === itemCompleto ? novoItem : valor
-                    );
-
-                    itemCompleto = novoItem;
-                }
-
-                item.classList.remove("editando");
-            };
-
-            // BOTÃO REMOVER
-            const botaoRemover = document.createElement("button");
-            botaoRemover.innerHTML = '<i class="fa-solid fa-trash"></i>';
-
-            botaoRemover.onclick = function(){
-                lista.removeChild(item);
-                Lista = Lista.filter(valor => valor !== itemCompleto);
-            };
-
-            item.appendChild(botaoEditar);
-            item.appendChild(botaoRemover);
-
-            lista.appendChild(item);
-
-            nomeInput.value = "";
-            qtdInput.value = "";
-        }
-        else{
-            alert("Esse item já está na lista!");
-        }
-
-    } else{
-        alert("Preencha produto e quantidade!");
+        renderizarLista();
     }
 }
+
+function renderizarLista(){
+
+    const ul = document.getElementById("lista");
+    ul.innerHTML = "";
+
+    let inicio = (paginaAtual - 1) * itensPorPagina;
+    let fim = inicio + itensPorPagina;
+
+    let itensPagina = lista.slice(inicio, fim);
+
+    itensPagina.forEach((item, index) => {
+
+        let li = document.createElement("li");
+
+        let span = document.createElement("span");
+        span.textContent = `${item.nome} - ${item.qtd}x`;
+
+        // EDITAR
+        let botaoEditar = document.createElement("button");
+        botaoEditar.innerHTML = '<i class="fa-solid fa-pen"></i>';
+
+        botaoEditar.onclick = function(){
+
+            li.classList.add("editando");
+
+            span.contentEditable = true;
+            span.focus();
+
+            span.onblur = function(){
+
+                let texto = span.textContent.split("-");
+                if(texto.length >= 2){
+                    item.nome = texto[0].trim();
+                    item.qtd = texto[1].replace("x","").trim();
+                }
+
+                span.contentEditable = false;
+                li.classList.remove("editando");
+            };
+        };
+
+        // REMOVER
+        let botaoRemover = document.createElement("button");
+        botaoRemover.innerHTML = '<i class="fa-solid fa-trash"></i>';
+
+        botaoRemover.onclick = function(){
+            lista.splice(inicio + index, 1);
+            renderizarLista();
+        };
+
+        li.appendChild(span);
+        li.appendChild(botaoEditar);
+        li.appendChild(botaoRemover);
+
+        ul.appendChild(li);
+    });
+
+    atualizarInfoPagina();
+}
+
+function atualizarInfoPagina(){
+    const totalPaginas = Math.ceil(lista.length / itensPorPagina) || 1;
+    document.getElementById("infoPagina").textContent =
+        `Página ${paginaAtual} de ${totalPaginas}`;
+}
+
+function proximaPagina(){
+    const totalPaginas = Math.ceil(lista.length / itensPorPagina);
+    if(paginaAtual < totalPaginas){
+        paginaAtual++;
+        renderizarLista();
+    }
+}
+
+function paginaAnterior(){
+    if(paginaAtual > 1){
+        paginaAtual--;
+        renderizarLista();
+    }
+}
+
+renderizarLista();
