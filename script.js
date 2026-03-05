a292b80a3a386815f8581f24d21d840d5746685f
Matheus <matheus.victor.ss48@gmail.com>
2026-03-05 10:39:00 -0300

    anotações de aprendizado

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//guarda itens tipo num JSON:
+//{
+// nome: "Arroz",
+// qtd: "2",
+// checked: false
+//}
 let lista = [];
 let paginaAtual = 1;
 const itensPorPagina = 8;
@@ -9,7 +15,7 @@ const controleMusica = document.getElementById('controleMusica');
 const iconeMusica = document.getElementById('iconeMusica');
 
 audioMusica.volume = 0.2;
-
+//ve se a musica ta tocando
 controleMusica.addEventListener('click', function() {
     if (musicaTocando) {
         audioMusica.pause();
@@ -22,7 +28,7 @@ controleMusica.addEventListener('click', function() {
     }
     musicaTocando = !musicaTocando;
 });
-
+//tenta tocar automaticamente no navegador
 window.addEventListener('load', function() {
     audioMusica.play().then(() => {
         musicaTocando = true;
@@ -33,18 +39,18 @@ window.addEventListener('load', function() {
         controleMusica.classList.add('musica-desligada');
     });
 });
-
+//quando for adicionar um item, puxa pelo id no DOM
 function adicionarItem(){
     let nomeInput = document.getElementById("nome");
     let qtdInput = document.getElementById("quantidade");
     let erro = document.getElementById("erro");
-
+//.trim remove espaços extras
     let nome = nomeInput.value.trim();
     let qtd = qtdInput.value.trim();
-
+//limpando msg de erro
     erro.style.display = "none";
     erro.textContent = "";
-
+//erro de campo vazio
     if(nome === "" || qtd === ""){
         erro.textContent = "Preencha o nome do produto e a quantidade! ✏️";
         erro.style.display = "block";
@@ -62,7 +68,7 @@ function adicionarItem(){
         erro.style.display = "block";
         return;
     }
-
+//deixa minusculo e confere se ja tem esse item
     let nomeLower = nome.toLowerCase();
     let existe = lista.some(item => item.nome.toLowerCase() === nomeLower);
 
@@ -71,24 +77,24 @@ function adicionarItem(){
         erro.style.display = "block";
         return;
     }
-
+//adiciona o item naquele primeiro array
     lista.push({
         nome: nome,
         qtd: qtd,
         checked: false
     });
-    
+    //limpa os inputs
     nomeInput.value = "";
     qtdInput.value = "";
 
     renderizarLista();
 }
-
+//marcar o item comprado pelo num do index
 function toggleCheck(index) {
     lista[index].checked = !lista[index].checked;
     renderizarLista();
 }
-
+//a função que é ativada no final de cada processo e atualiza a lista
 function renderizarLista(){
     const ul = document.getElementById("lista");
     ul.innerHTML = "";
@@ -96,27 +102,27 @@ function renderizarLista(){
     let inicio = (paginaAtual - 1) * itensPorPagina;
     let fim = inicio + itensPorPagina;
     let itensPagina = lista.slice(inicio, fim);
-
+//percorre os itens da página e calcula o indice do item adicionado e cria um li
     itensPagina.forEach((item, indexLocal) => {
         let indexReal = inicio + indexLocal;
         let li = document.createElement("li");
-        
+        //se esta sendo editado cria os campos de edição
         if (itemEditando === indexReal) {
             li.classList.add("editando");
-            
+            //editando o nome
             let inputNome = document.createElement("input");
             inputNome.type = "text";
             inputNome.value = item.nome;
             inputNome.classList.add("input-edicao-nome");
             inputNome.maxLength = 30;
-
+            //a quantidade
             let inputQtd = document.createElement("input");
             inputQtd.type = "number";
             inputQtd.value = item.qtd;
             inputQtd.min = "1";
             inputQtd.max = "999";
             inputQtd.classList.add("input-edicao-qtd");
-
+            //salvar o index, nom e qnt
             inputNome.addEventListener("keypress", function(e) {
                 if (e.key === "Enter") salvarEdicao(indexReal, inputNome, inputQtd);
             });
@@ -124,7 +130,7 @@ function renderizarLista(){
             inputQtd.addEventListener("keypress", function(e) {
                 if (e.key === "Enter") salvarEdicao(indexReal, inputNome, inputQtd);
             });
-
+            //appendichild coloca esse nó no final do pai
             li.appendChild(inputNome);
             li.appendChild(inputQtd);
             
@@ -136,11 +142,11 @@ function renderizarLista(){
             };
             
             li.appendChild(botaoConfirmar);
-            
+            //se nao estiver editando deixa os botões padrão
         } else {
             let checkboxWrapper = document.createElement("div");
             checkboxWrapper.classList.add("checkbox-wrapper");
-            
+            //checkbox
             let checkbox = document.createElement("input");
             checkbox.type = "checkbox";
             checkbox.checked = item.checked || false;
@@ -149,23 +155,23 @@ function renderizarLista(){
             };
             
             checkboxWrapper.appendChild(checkbox);
-
+//container
             let nomeContainer = document.createElement("div");
             nomeContainer.classList.add("nome-container");
-
+//nome
             let spanNome = document.createElement("span");
             spanNome.classList.add("nome-produto");
             spanNome.textContent = item.nome;
             
             nomeContainer.appendChild(spanNome);
-
+//quantidade
             let spanQtd = document.createElement("span");
             spanQtd.classList.add("quantidade");
             spanQtd.textContent = item.qtd + "x";
 
             let botoesContainer = document.createElement("div");
             botoesContainer.classList.add("botoes-container");
-
+//botão editar
             let botaoEditar = document.createElement("button");
             botaoEditar.innerHTML = '<i class="fa-solid fa-pen"></i>';
             botaoEditar.classList.add("botao-editar");
@@ -173,14 +179,15 @@ function renderizarLista(){
                 itemEditando = indexReal;
                 renderizarLista();
             };
-
+//bbotão remover
             let botaoRemover = document.createElement("button");
             botaoRemover.innerHTML = '<i class="fa-solid fa-trash"></i>';
             botaoRemover.classList.add("botao-remover");
             botaoRemover.onclick = function() {
+                //remove item do array
                 lista.splice(indexReal, 1);
                 itemEditando = null;
-
+//ve se tem item a ser excluido e remove ele
                 if((paginaAtual - 1) * itensPorPagina >= lista.length && paginaAtual > 1){
                     paginaAtual--;
                 }
@@ -206,7 +213,7 @@ function renderizarLista(){
 
     atualizarInfoPagina();
 }
-
+//salvar
 function salvarEdicao(index, inputNome, inputQtd) {
     if (!inputNome || !inputQtd) return;
     
@@ -215,6 +222,7 @@ function salvarEdicao(index, inputNome, inputQtd) {
     
     if (novoNome && novoQtd) {
         if (novoNome.length <= 30 && parseInt(novoQtd) <= 999) {
+            //atualiza os novos valores
             lista[index].nome = novoNome;
             lista[index].qtd = novoQtd;
         }
@@ -223,8 +231,9 @@ function salvarEdicao(index, inputNome, inputQtd) {
     itemEditando = null;
     renderizarLista();
 }
-
+//numero de paginas
 function atualizarInfoPagina(){
+    //função math calcula numero de paginas e arredonda para cima
     const totalPaginas = Math.ceil(lista.length / itensPorPagina) || 1;
     document.getElementById("numeroPagina").textContent = paginaAtual;
     document.getElementById("btnVoltar").style.display = paginaAtual > 1 ? "flex" : "none";
@@ -234,6 +243,7 @@ function atualizarInfoPagina(){
 function proximaPagina(){
     const totalPaginas = Math.ceil(lista.length / itensPorPagina);
     if(paginaAtual < totalPaginas){
+        //aumenta a pagina em 1
         paginaAtual++;
         renderizarLista();
     }
@@ -241,6 +251,7 @@ function proximaPagina(){
 
 function paginaAnterior(){
     if(paginaAtual > 1){
+        //diminui a pagina em 1
         paginaAtual--;
         renderizarLista();
     }
